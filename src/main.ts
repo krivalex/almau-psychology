da8b4f5fb4a3a995ea9c7a73f8ee0516b3c54e65
krivalex <aleksei_krivoborodov@mail.ru>
2024-01-21 17:12:03 +0600

    structure base upd

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,14 @@ import 'primeicons/primeicons.css'
 import App from './App.vue'
 import router from './router'
 
-const firebaseConfig = {}
+const firebaseConfig = {
+  apiKey: 'AIzaSyAevhsUClJR9mSwS8usXVJYIiNeQ-iMkGY',
+  authDomain: 'almau-psychology.firebaseapp.com',
+  projectId: 'almau-psychology',
+  storageBucket: 'almau-psychology.appspot.com',
+  messagingSenderId: '381800203653',
+  appId: '1:381800203653:web:4417ac03aee51c3c0df0ec',
+}
 
 initializeApp(firebaseConfig)
 const app = createApp(App)
