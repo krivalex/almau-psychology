bb273eb7ecd384cd2238c9a7f5b2a5b4c1bd5683
krivalex <new.krivalex@gmail.com>
2024-04-21 15:42:31 +0600

    fixed text

--- src/composables/useUserDevice.ts
+++ src/composables/useUserDevice.ts
@@ -9,11 +9,11 @@ export const useUserDevice = () => {
 
   const textForIOS = {
     title: 'Похоже у вас iOS устройство и вы здесь впервые',
-    description: 'IOS система установленая на вашем устройстве не поддерживает технологию WebViewApi 3D Secure, мы перебросили вас в ваш основной браузер для регистрации',
+    description: 'Нажмите на кнопку, перейти в браузер - для регистрации',
   }
   const textForAndroid = {
     title: 'Похоже у вас Android устройство и вы здесь впервые',
-    description: 'Android система установленая на вашем устройстве не поддерживает технологию WebViewApi 3D Secure, мы перебросили вас в ваш основной браузер для регистрации',
+    description: 'Нажмите на кнопку, перейти в браузер - для регистрации',
   }
 
   const isBrowserMounted = () => isWindows()
