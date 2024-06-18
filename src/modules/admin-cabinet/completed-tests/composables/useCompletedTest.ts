97a32560628b5227ddd77a5aa8b78259d2d7ec14
@krivoborodov_56336 <alexey.krivoborodov@kaspi.kz>
2024-06-18 21:15:27 +0500

    fix

--- src/modules/admin-cabinet/completed-tests/composables/useCompletedTest.ts
+++ src/modules/admin-cabinet/completed-tests/composables/useCompletedTest.ts
@@ -35,10 +35,6 @@ export function useCompletedTest() {
   }
 
   function exportPretify(event: { data: any; field: string }) {
-    if (event.field === 'student') {
-      event.data.name = `${event?.data?.student?.firstName}`
-      event.data.surname = `${event?.data?.student?.lastName}`
-    }
     if (event.field === 'status') {
       event.data =
         event.data.status === 'not_started'
